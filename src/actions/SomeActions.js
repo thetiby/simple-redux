22b07b885a26d175f036fdfd25a9642622260393
Tiby <tiberiu.chetreanu@trisoft.ro>
2016-03-24 19:52:54 +0200

    added some comments; fixed typo

--- src/actions/SomeActions.js
+++ src/actions/SomeActions.js
@@ -2,7 +2,7 @@ import newAction from './actionCreatorFactory';
 
 let actions = {};
 
-newAction('RESET_STATE', actions);
+newAction('RESET_STATE', actions); // => will generate actions.resetState
 newAction('ANOTHER_ACTION', actions);
 
 export default actions;
